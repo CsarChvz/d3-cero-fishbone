84f175284604b854616b9a9d4086508cb08af661
Cesar Chavez Rodriguez <79390377+CsarChvz@users.noreply.github.com>
2023-11-16 16:50:30 -0600

    q pasas si

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { testData, testData1, testData2, testData3 } from './fish/test.data';
-
+import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    this.variable = environment.pencil;
+  }
+  variable: string;
   title = 'd3-cero-fishbone';
   data = testData1;
 }
